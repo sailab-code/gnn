9116fa3c51a5a88e543b23106cadad1888a9bdc9
Matteo <tiezzi7matteo@gmail.com>
2019-06-05 17:03:08 +0200

    req

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gnn",
-    version="1.0.4",
+    version="1.0.5",
     author="Matteo Tiezzi",
     author_email="mtiezzi@diism.unisi.it",
     description="Graph Neural Network Tensorflow implementation",
